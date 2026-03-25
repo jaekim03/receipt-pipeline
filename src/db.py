f7e0e50e0e1deb7508cd1ca8cbdc4b9d68fc0263
Jaeha Kim <jhk@Jaehas-MacBook-Pro.local>
2026-03-24 21:25:35 -0700

    Build Database Connection

---
+++ src/db.py
@@ -0,0 +1,14 @@
+import os 
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        port=os.environ["POSTGRES_PORT"],
+        dbname=os.environ["POSTGRES_DB"],
+        user=os.environ["POSTGRES_USER"],
+        password=os.environ["POSTGRES_PASSWORD"]
+    )
